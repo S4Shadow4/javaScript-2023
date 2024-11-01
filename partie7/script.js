bcc5a44c15b838b7fe24250c52d64de66811df6e
YannAGB <=>
2024-11-01 12:51:05 +0000

    ajout de la partie7

---
+++ partie7/script.js
@@ -0,0 +1,46 @@
+
+//fonction fléchée
+function add(){
+    return 2 + 4
+};
+console.log(add())
+
+var add1 =()=>{
+    return 2+4
+}
+console.log(add1())
+
+let add2 = (n1,n2)=>(n1+n2)
+console.log(add2(2,4))
+
+
+//Spread operator
+
+let colors = ['red', 'green', 'blue']
+
+let colorAdd = colors.concat('pink')
+console.log(colorAdd)
+
+let newColors = [...colors,'green']
+console.log(newColors)
+
+//utilisation du mot clé let pour déclarer les variables en local
+var name = 'Yann'
+
+if(1 === 1){
+    var name = 'Jacob'
+}
+console.log(name)
+
+//litteral template
+//destructuring
+
+let person = {
+    nom : 'jules',
+    age : 12
+}
+
+let personnage =({nom,age})=>(
+    console.log(`je me nomme ${nom} et j'ai ${age}`)
+)
+personnage(person)
